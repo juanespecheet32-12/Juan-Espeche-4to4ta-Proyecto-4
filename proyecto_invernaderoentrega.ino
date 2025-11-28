908f01d22f7ae79566b89f4c059e668a387e0cdf
juanespeche_et32 <juan.especheet32@gmail.com>
2025-11-28 14:29:52 -0300

    Add files via upload

---
+++ proyecto_invernaderoentrega.ino
@@ -0,0 +1,96 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+int tmp = 0;
+int humedad = 0;
+float temperatura = 0;
+int celsius;
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  tmp = analogRead(A0);
+  celsius = map(((tmp - 20) * 3.04), 0, 1023, -40, 125);
+  Serial.println(celsius);
+  humedad = analogRead(A1);
+  if(celsius > 32){
+    lcd.setCursor(0,0);
+    lcd.print("La temperatura");
+    lcd.setCursor(0,1);
+    lcd.print("es: ");
+    lcd.print(celsius);
+    delay(5000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("La planta");
+    lcd.setCursor(0,1);
+    lcd.print("necesita riego");
+  }
+  else if(celsius < 32){
+    lcd.setCursor(0,0);
+    lcd.print("La temperatura");
+    lcd.setCursor(0,1);
+    lcd.print("es: ");
+    lcd.print(celsius);
+    delay(5000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("La temperatura");
+    lcd.setCursor(0,1);
+    lcd.print("es estable");
+    delay(5000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("La planta no");
+    lcd.setCursor(0, 1);
+    lcd.print("necesita Riego");
+    delay(5000);
+    lcd.clear();
+  }
+  if (humedad < 87){
+    lcd.setCursor(0,0);
+    lcd.print("La humedad es ");
+    lcd.setCursor(0,1);
+    lcd.print("menor al 10%");
+    delay(5000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("La planta");
+    lcd.setCursor(0, 1);
+    lcd.print("necesita riego");
+    delay(5000);
+    lcd.clear();
+  }else if(humedad > 306){
+    lcd.setCursor(0,0);
+    lcd.print("La humedad es:");
+    lcd.setCursor(0, 1);
+    lcd.print("mayor al 30%");
+    delay(5000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("La planta no");
+    lcd.setCursor(0, 1);
+    lcd.print("necesita riego");
+    delay(5000);
+    lcd.clear();
+  }else{
+    lcd.setCursor(0,0);
+    lcd.print("La humedad es: ");
+    lcd.setCursor(0, 1);
+    lcd.print(humedad);
+    delay(5000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("La planta");
+    lcd.setCursor(0, 1);
+    lcd.print("necesita riego");
+    delay(5000);
+    lcd.clear();
+  }
+}
